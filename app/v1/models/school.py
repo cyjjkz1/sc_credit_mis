204bf3a36173a0878f86505dbcdd30a12c6b6f2b
cyjjkz1 <cyjjkz1@sina.com>
2019-03-06 14:15:30 +0800

    add model

--- app/v1/models/school.py
+++ app/v1/models/school.py
@@ -52,3 +52,32 @@ class Major(db.Model):
             'create_time': self.create_time
         }
         return maj_dict
+
+
+class AuditDepartment(db.Model):
+    __table__ = 'audit_department'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(16), nullable=False)
+    create_time = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    projects = db.relationship('Project', backref='audit_department', lazy='dynamic')
+
+
+class Category(db.Model):
+    __table__ = 'category'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(16), nullable=False)
+    create_time = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    projects = db.relationship('Project', backref='category', lazy='dynamic')
+
+
+class Project(db.Model):
+    __table__ = 'project'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(16), nullable=False)
+    detail = db.Column(db.String(200), nullable=False)
+    max_credit = db.Column(db.Integer, nullable=False)
+    min_credit = db.Column(db.Integer, nullable=False)
+    create_time = db.Column(db.DateTime, nullable=False, default=datetime.now)
+
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    department_id = db.Column(db.Integer, db.ForeignKey('audit_department.id'))
