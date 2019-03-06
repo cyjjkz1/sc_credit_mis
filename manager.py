e15a29cdf717a353fa3e0ca599e9fab995774881
cyjjkz1 <cyjjkz1@sina.com>
2019-03-06 10:28:13 +0800

    修改DB

--- manager.py
+++ manager.py
@@ -7,8 +7,10 @@ from flask_script import Manager, Command, Server
 from flask_migrate import Migrate, MigrateCommand
 
 
+from tests.school_db_init import AddDB
+
 # import db model
-from app.v1 import models
+#from app.v1 import models
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -21,19 +23,6 @@ migrate = Migrate(app, db)
 class CreateDB(Command):
     def run(self):
         db.create_all()
-
-
-class AddDB(Command):
-    def run(self):
-        col_dict = models.school.College('外语学院')
-        col_dict1 = models.school.College('物理学院')
-        col_dict2 = models.school.College('生物科学学院')
-        db.session.add(col_dict1)
-        db.session.add(col_dict2)
-        db.session.add(col_dict)
-        db.session.commit()
-
-
 
 # 自定义命令
 manager.add_command('createdb', CreateDB)
--- tests/school_db_init.py
+++ tests/school_db_init.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from app import db, create_app
-from flask_script import Manager, Command, Server
-from flask_migrate import Migrate, MigrateCommand
-from ..app.v1 import models
+from app import db
+from flask_script import Command
+from app.v1.models.school import College, Major
 
 
 class AddDB(Command):
     def run(self):
-        col_dict = models.school.College('外语学院')
-        col_dict1 = models.school.College('物理学院')
-        col_dict2 = models.school.College('生物科学学院')
-        db.session.add(col_dict1)
-        db.session.add(col_dict2)
-        db.session.add(col_dict)
+        cols = College.query.all()
+        db.session.delete(cols)
         db.session.commit()
 
 
