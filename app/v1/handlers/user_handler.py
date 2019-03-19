a0c57a893076e6acbad94a1c0baa141421748a62
cyjjkz1 <cyjjkz1@sina.com>
2019-03-19 10:00:58 +0800

    修改session删除

--- app/v1/handlers/user_handler.py
+++ app/v1/handlers/user_handler.py
@@ -62,8 +62,10 @@ class LoginHandler(BaseHandler):
                     raise HandlerException(respcd=RESP_CODE.USER_NOT_LOGIN, respmsg='用户角色错误')
 
                 # 密码正确，可以打cookie
-                if user.session.first() is not None:
-                    db.session.delete(user.session.first())
+                origin_session = user.session
+                app.logger.info('db query origin session = %s'.format(origin_session))
+                if origin_session is not None:
+                    db.session.delete(origin_session)
                     db.session.commit()
                 new_session_id = self.create_session_id()
                 self.session_id = new_session_id
